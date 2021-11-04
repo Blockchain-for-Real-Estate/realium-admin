75e8337ce8b7d603942f95a22197a70871b60c9e
Rob Upchurch <rob@axialabs.com>
2021-11-03 21:47:51 -0700

    Update properties page

--- src/pages/properties/PropertiesPage.js
+++ src/pages/properties/PropertiesPage.js
@@ -2,7 +2,6 @@ import useProperties from "context/queries/useProperties";
 
 const PropertiesPage = () => {
   const { data: properties } = useProperties();
-  if (properties) debugger;
   return <div>PropertiesPage</div>;
 };
 
